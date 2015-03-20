ee832940c7e793c0a88ae79014e7b6c375af3d80
Jack McDevitt <jackmcdevitt4@gmail.com>
2015-03-20 15:50:02 -0400

    "reject completed with use of filter"

--- src/underbar.js
+++ src/underbar.js
@@ -89,6 +89,7 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+  return _.filter(collection, function(num){if(!test(num)){return num}})
   };
 
   // Produce a duplicate-free version of the array.
